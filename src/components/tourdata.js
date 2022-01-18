68ba467d84b25acc8b187449f1ed9e0e17eeb866
unknown <kobbyokai18@gmail.com>
2022-01-18 14:24:41 +0000

    changed images

--- src/components/tourdata.js
+++ src/components/tourdata.js
@@ -1,35 +1,99 @@
 export const tourData = [
     {
         id : 1,
-        city : 'usa',
-        img : "https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        // img : './image/pic1.jpg',
-        name : 'usa pic',
-        details : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam nobis ducimus vero dolor atque blanditiis nihil praesentium amet nam velit. Illo facilis dignissimos officiis, consequuntur harum libero soluta ea expedita.'
+        city : 'USA',
+        img : "https://th.bing.com/th/id/OIP.ZEOHFcFvJnPWVqxz1MBsTgHaEK?w=303&h=180&c=7&r=0&o=5&pid=1.7",
+        name : 'the white house',
+        details : 'As the longtime home of the U.S. president and the location of countless momentous decisions and historic moments, 1600 Pennsylvania Avenue is immediately identifiable and familiar to any American—and plenty of non-Americans, too. But as well as you know it, how well do you really know the White House?'
     },
 
     {
         id : 2,
         city : 'ghana',
-        img : './image/pic2.jpg',
-        name : 'china pic',
-        details : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam nobis ducimus vero dolor atque blanditiis nihil praesentium amet nam velit. Illo facilis dignissimos officiis, consequuntur harum libero soluta ea expedita.'
+        img : "https://th.bing.com/th/id/OIP.l1l0W6HezwISKq_WNLTRnQHaFO?w=235&h=180&c=7&r=0&o=5&pid=1.7",
+        name : 'aburi botanical gardens',
+        details : 'Aburi is located on the Akwapim-Togo Range of Ghana. It’s just three quarters of an hour drive from Accra, the capital of Ghana. The cool mountainous weather of Aburi makes it a destination for people who love the cool side of life. Located in this cool tranquil environment is the Aburi Botanical Gardens. The garden covers a total land area of about one hundred and sixty (160) acres. However it is only three (3) acres that have been developed and the remaining serving as a botanical reserve.'
     },
 
     {
         id : 3,
         city : 'greece',
-        img : './image/pic3.jpg',
-        name : 'greece pic',
-        details : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam nobis ducimus vero dolor atque blanditiis nihil praesentium amet nam velit. Illo facilis dignissimos officiis, consequuntur harum libero soluta ea expedita.'
+        img : 'https://th.bing.com/th/id/OIP.Vf2uDrZz38o6zbfvathfywHaEo?w=267&h=180&c=7&r=0&o=5&pid=1.7',
+        name : 'beach of athens',
+        details : 'Greece , officially the Hellenic Republic, is a country located in Southeastern Europe. Its population is approximately 10.7 million as of 2018; Athens is its largest and capital city, followed by Thessaloniki. Situated on the southern tip of the Balkans, Greece is located at the crossroads of Europe'
     },
 
     {
         id : 4,
-        city : 'hong kon',
-        img : './image/pic1.jpg',
-        name : 'hon pic',
-        details : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam nobis ducimus vero dolor atque blanditiis nihil praesentium amet nam velit. Illo facilis dignissimos officiis, consequuntur harum libero soluta ea expedita.'
+        city : 'hong kong',
+        img : 'https://th.bing.com/th/id/OIP.51Xpc6g_bk7_JhfW_iYyZwHaE8?w=274&h=183&c=7&r=0&o=5&pid=1.7',
+        name : 'city of hong kong',
+        details : 'Hong Kong lies at the northern fringe of the tropical zone. Its monsoonal (wet-dry) seasonal changes are well marked, however, with hot, humid summers and cool, dry winters. The climate is largely controlled by the atmospheric pressure systems over the adjacent great Asian landmass and ocean surface. '
+    },
+
+    {
+        id : 5,
+        city : 'paris',
+        img : 'https://th.bing.com/th/id/OIP.PR6o1VSaEc20IXq7gLcHrgHaEK?w=185&h=104&c=7&r=0&o=5&pid=1.7',
+        name : 'eiffel tower',
+        details : 'Paris, city and capital of France, situated in the north-central part of the country. People were living on the site of the present-day city, located along the Seine River some 233 miles (375 km) upstream from the river’s mouth on the English Channel (La Manche), by about 7600 BCE. The modern city has spread from the island (the Île de la Cité) and far beyond both banks of the Seine.'
+    },
+
+    {
+        id : 6,
+        city : 'hawaii',
+        img : 'https://th.bing.com/th/id/OIP.we1lR4beZuz7VePYZoD1_AHaE7?w=278&h=185&c=7&r=0&o=5&pid=1.7',
+        name : 'beach of hawaii',
+        details : 'Hawaii, constituent state of the United States of America. Hawaii (Hawaiian: Hawai‘i) became the 50th U.S. state on August 21, 1959. Hawaii is a group of volcanic islands in the central Pacific Ocean. The islands lie 2,397 miles (3,857 km) from San Francisco, California, to the east and 5,293 miles (8,516 km) from Manila, in the Philippines, to the west. The capital is Honolulu, located on the island of Oahu.'
+    },
+
+    {
+        id : 7,
+        city : 'Spain',
+        img : 'https://th.bing.com/th/id/OIP.sZmKJ8y1_bMuj9tbBWNWTAHaE8?w=282&h=188&c=7&r=0&o=5&pid=1.7',
+        name : 'baeleric islands',
+        details : 'Spain is a storied country of stone castles, snowcapped mountains, vast monuments, and sophisticated cities, all of which have made it a favoured travel destination. The country is geographically and culturally diverse. Its heartland is the Meseta, a broad central plateau half a mile above sea level.'
+    },
+
+    {
+        id : 8,
+        city : 'united kingdom',
+        img : 'https://th.bing.com/th/id/OIF.c9rFO2kOiWRNXccqKxsKPw?w=302&h=181&c=7&r=0&o=5&pid=1.7',
+        name : 'london bridge',
+        details : 'Several bridges named London Bridge have spanned the River Thames between the City of London and Southwark, in central London. The current crossing, which opened to traffic in 1973, is a box girder bridge built from concrete and steel. It replaced a 19th-century stone-arched bridge'
+    },
+
+
+    {
+        id : 9,
+        city : 'russia',
+        img : "https://th.bing.com/th/id/OIP.KakZSAFLvVUt9L0sIHBOLQHaEK?w=255&h=180&c=7&r=0&o=5&pid=1.7",
+        name : 'Russia tower',
+        details : 'Russia has been described as many things: a mother, a winner, a force and a fighter. It is, as Churchill put it famously, a riddle wrapped in a mystery inside an enigma.'
+    },
+
+    {
+        id : 10,
+        city : 'germany-hamburg',
+        img : 'https://th.bing.com/th/id/OIP.5zWASKvx9VcuWrvtkxnsWgHaE8?w=258&h=180&c=7&r=0&o=5&pid=1.7',
+        name : 'hamburg',
+        details : 'Hamburg is Germany\'s second-largest economic center, Germany\'s most busy port and significant industrial metropolis. Today Hamburg is an attractive, contemporary city and cultural center, internationally renowned for its opera, theatre, magazines and libraries, radio, television and film studios'
+    },
+
+    {
+        id : 11,
+        city : 'portugal',
+        img : 'https://th.bing.com/th/id/OIP.xoTKY73U-t1T5LSbY4DCRQHaFb?w=230&h=180&c=7&r=0&o=5&pid=1.7',
+        name : 'belem tower',
+        details : 'The Belém Tower is situated on the northern bank of the Tagus River in the civil parish of Santa Maria de Belém, municipality of Lisbon, accessible at the western end of the Avenida de Brasília by a small bridge. Nearby are the Jeronimos Monasteryto the east and the Forte do Bom Sucesso to the west, while to the north are the tower Governor\'s residence'
+    },
+
+    {
+        id : 12,
+        city : 'canada',
+        img : 'https://th.bing.com/th/id/OIP.1U-SgJbA0SV8wDBSUbOC3wAAAA?w=258&h=180&c=7&r=0&o=5&pid=1.7',
+        name : 'city of toronto',
+        details : 'Toronto, the capital of the province of Ontario, is a major Canadian city along Lake Ontario\'s northwestern shore. It\'s a dynamic metropolis with a core of soaring skyscrapers, all dwarfed by the iconic, free-standing CN Tower. Toronto also has many green spaces, from the orderly oval of Queen’s Park to 400-acre High Park and its trails, sports facilities and zoo'
     },
 
     
